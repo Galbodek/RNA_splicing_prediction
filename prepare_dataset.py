e3ac58462ae8d92ff00f28210fd70af84d7d705d
Ella Rannon <ellarannon@power9login.tau.ac.il>
2023-09-14 10:42:52 +0300

    added filteration per ratio of 0 and 1

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,23 +1,32 @@
 import json
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 
-data_file = "/sternadi/nobackup/volume1/ellarannon/splicing/data_fil.json"
-chunk_size = 1000
-
-with open(data_file, 'r') as fin:
-	all_data = json.load(fin)
+def remove_outliers(data, percentage=0.02):
+	print(np.median([sum(d['coding_seq']) / len(d['coding_seq']) for d in data]))
+	return [d for d in data if 1-percentage >= (sum(d['coding_seq']) / len(d['coding_seq'])) >= percentage]
 
 
 def split_data(data, chunk_size, prefix):
 	for i in range(0, len(data), chunk_size):
 		sd = {'data': data[i:i+chunk_size]}
-        file_name = f'./data/{prefix}_data_file_{int(i/chunk_size)}.json'
-        with open(file_name, 'w') as fout:
-        	json.dump(sd, fout)
+		file_name = f'/sternadi/nobackup/volume1/ellarannon/splicing/data/{prefix}_data_file_{int(i/chunk_size)}.json'
+		with open(file_name, 'w') as fout:
+			json.dump(sd, fout)
 
 
-train, test = train_test_split(d, test_size=0.2, random_state=42)
+
+data_file = "/sternadi/nobackup/volume1/ellarannon/splicing/encoded_transcripts.json"
+chunk_size = 500
+
+with open(data_file, 'r') as fin:
+	all_data = json.load(fin)
+
+print(len(all_data))
+all_data = remove_outliers(all_data)
+print(len(all_data))
+train, test = train_test_split(all_data, test_size=0.2, random_state=42)
 val, test = train_test_split(test, test_size=0.5, random_state=42)
 split_data(train, chunk_size, 'train')
 split_data(val, chunk_size, 'val')
